5fe0eb6bc1c2921e0aa17d6b34c1108e3e88cab1
lindsayagreen <lindsgree@gmail.com>
2023-10-14 13:04:25 -0500

    update

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var apikey ="b25abf6e18262639c4d411b925801b5d"
 
-var userInput = document.getElementById("input");
-var searchBtn = document.getElementById("btn");
+var userInput = $('#input');
+var searchBtn = $('#btn');
+var currentHour = parseInt(dayjs().format("HH"));
+var currentWeather= $('#currentWeather');
+var searchResults= $('#searchResults');
+var row2= $('#row2')
 
-searchBtn.addEventListener("click", function(){
+searchBtn.on("click", function(){
 
-    console.log(userInput.value)
-    getWeather(userInput.value)
+    console.log(userInput.val())
+    getWeather(userInput.val())
 });
 
 function getWeather (city) {
@@ -19,23 +23,42 @@ function getWeather (city) {
     })
     .then(function (data) {
       console.log(data)
-      getForcast(data.coord.lat, data.coord.lon)
+      getForcast(data)
       var city = document.getElementById("city")
       city.textContent= data.name
+      currentWeather.append(data.name + ' ('+dayjs.unix(data.dt).format('MM/DD/YYYY')+') <img src="https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png">');
+      currentWeather.append('<p>Temperature:'+data.main.temp+'</p>')
+      currentWeather.append('<p>Humidity: '+data.main.humidity+'</p>')
+      currentWeather.append('<p>Wind: '+data.wind.speed+'</p>')
     });
 }
 
-function getForcast (lat, lon) {
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ lon + '&appid=' + apikey + '&units=imperial';
-
-fetch(requestUrl)
-.then(function (response) {
-    console.log (response)
-  return response.json();
-})
-.then(function (data) {
-  console.log(data)
-  $()
-});
-}
+function getForcast (data) {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+data.name+'&appid=' + apikey + '&units=imperial';
+  fetch(requestUrl)
+  .then(function (response) {
+      console.log (response)
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+    row2.empty();
+    for (let i = 2; i < data.list.length; i+=8) {
+      console.log(data.list[i])
+      row2.append(` <div class="col">
+      <div class="card" id="monday">
+        <div class="card-body">
+          <h5 class="card-title">${dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')}</h5>
+          <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Humidity: <span class="humidity">${data.list[i].main.humidity}</span></li>
+            <li class="list-group-item">Temperature:<span class="temperature">${data.list[i].main.temp}</span></li>
+            <li class="list-group-item">Wind Speed: <span class="windspeed">${data.list[i].wind.speed}</span></li></li>
+          </ul>
+        </div>
+      </div>
+    </div>`)
+    }
+  });
+  };
 
